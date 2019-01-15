f1fb30329590a61a571b37692f286f345c67f7e7
Greg Hemphill <greg@webstop.com>
2019-01-14 23:31:27 -0500

    Adds options &, attributes from a file

--- README.md
+++ README.md
@@ -8,3 +8,68 @@ There is a command line interface (CLI), which allows you to run the scaffold ge
 and view the help documentation.
 
 
+## Supplying Attribute data
+
+Copy the `data/_example_attributes.yml` and rename it replacing `_example` with the name of your Lasso Module. For 
+example, if your module was called `coupon`, name the file `coupon_attributes.yml`. 
+
+### Attribute Data File Format
+
+The attribute data is a YAML file, and is formatted like the following example:
+
+```yaml
+
+---
+-
+  name: address
+  field: Address1
+  type: String
+  usage: required
+-
+  name: unit
+  field: Address2
+  type: String
+  usage: optional
+```
+
+This produces the following in the Ruby code:
+
+```ruby
+@attributes = [
+  {
+    name: 'address',
+    field: 'Address1',
+    type: 'String',
+    usage: 'required' 
+  },
+  {
+    name: 'unit',
+    field: 'Address2',
+    type: 'String',
+    usage: 'optional' 
+  }
+]
+```
+
+#### Data Fields
+
+- `name` is the name of your attribute. Example: `store_number`.
+- `field` is the Lasso database field it maps to. Example: 'StoreNbr'.
+- `type` is the Lasso data type to treat the attribute as. Examples: `string`, `integer`, or `boolean`.
+- `usage` identifies how we use the attribute. It takes one of the following values: `required`, `optional`, `hidden`, `internal`, or `virtual`
+  - `required`: accept input, in db
+  - `optional`: accept input, in db
+  - `hidden`:   no input,     in db
+  - `internal`: no input,     no db
+  - `virtual`:  accept input, no db
+
+##### Usage Values
+
+| Name       | Allows Input | Persists to Database |
+|------------|--------------|----------------------|
+| `required` | yes          | yes                  |
+| `optional` | yes          | yes                  |
+| `hidden`   | no           | yes                  |
+| `internal` | no           | no                   |
+| `virtual`  | yes          | no                   |
+
--- generator.rb
+++ generator.rb
@@ -2,52 +2,77 @@ require 'thor'
 require 'action_view'
 require 'active_support'
 require 'active_support/core_ext'
+require 'yaml'
+# require 'optparse'
+# require 'commandline/optionparser'
+
 
 class ScaffoldGeneratorCLI < Thor
   include Thor::Actions
   include ActionView::Helpers::TextHelper
+  #include CommandLine
+  
   #include ActiveSupport::Inflector
 
   attr_accessor :name
-  class_option :verbose, type: :boolean, default: true
+  #class_option :verbose, type: :boolean, default: true
 
   def self.source_root
     File.dirname(__FILE__)
   end
-
-  
-
-  # desc "hello NAME", "say hello to NAME"
-  # options :from => :required, :yell => :boolean
-  # def hello(name, from=nil)
-  #   puts "from: #{from}" if from
-  #   puts "Hello #{name}"
-  # end
-  
-  desc 'scaffold NAME (e.g. `digital_coupons`)', 'Creates a file structure & base set of files for Lasso Modules.'
-  def scaffold(name)
-    set_supporting_data(name)
-    directory "source_files", "output/#{@plural_name}", :exclude_pattern => /.DS_Store/
-    #template 'source_files/README.md.tt', "output/#{@plural_name}/README.md"
+     
+  desc 'scaffold NAME', 'Creates a file structure & base set of files for Lasso Modules.'
+  method_option :namespace, type: :string, default: 'grocery', aliases: '-n'
+  method_option :help, type: :boolean, default: false
+  def scaffold(name=nil)
+    if options[:help]
+      exec('ruby ./generator.rb help scaffold')
+    elsif name.nil?
+      puts 'ERROR: Name is required, but was left blank. Please try again and supply a name for your Lasso Module.'
+    else
+      set_supporting_data(name)
+      directory "source_files", "output/#{@plural_name}", :exclude_pattern => /.DS_Store/
+    end
   end
 
-  # desc 'singular_name', 'Used for creating custom file names.'
-  # Must be a public method or the directory method won't have access to it.
-  def singular_name
-    @singular_name
-  end
+  no_commands {
+    desc nil, 'Used for creating custom file names.'
+    # Used for creating custom file names.
+    # Must be a public method or the directory method won't have access to it.
+    def singular_name
+      @singular_name
+    end
 
-  # desc 'plural_name', 'Used for creating custom file names.'
-  # Must be a public method or the directory method won't have access to it.
-  def plural_name
-    @plural_name
-  end
+    desc nil, 'Used for creating custom file names.'
+    # Must be a public method or the directory method won't have access to it.
+    def plural_name
+      @plural_name
+    end
+  }
   
   private
   
   def set_supporting_data(name)
+    @namespace = options[:namespace]
+    @namespace += '_' unless @namespace.end_with?('_')
     @singular_name = name
     @plural_name = ActiveSupport::Inflector.pluralize(name)
+    @attributes = YAML.load(File.read("data/#{@singular_name}_attributes.yml"))
+    
+    names = []
+    @attributes.each do |attr|
+      names << attr['name']
+    end
+    @largest_name = largest(names)
+    puts "@largest_name: #{@largest_name}"
+  end
+  
+  def largest(items)
+    size = 0
+    items.each do |item|
+      size = item.size if item.size > size
+    end
+    size
   end
   
 end
